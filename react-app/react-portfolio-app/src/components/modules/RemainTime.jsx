27b30e980a767abebdeb2e416b8de6fc20c6fdc5
itjuh <jh.2144.9679@gmail.com>
2024-04-16 00:01:57 +0900

    Update RemainTime.jsx

--- react-app/react-portfolio-app/src/components/modules/RemainTime.jsx
+++ react-app/react-portfolio-app/src/components/modules/RemainTime.jsx
@@ -4,16 +4,16 @@ export function RemainTime({timeSts}) {
     // 남은시간
     const [remainT, setRemainT] = useState(20);
 
-    // useEffect(()=>{
-    //     const autoI = setInterval(()=>{
-    //         setRemainT((remainT)=>remainT - 1);
-    //     },1000);
-    //     if(remainT == 0){
-    //         clearInterval(autoI);
-    //         timeSts();
-    //     }
-    //     return ()=> clearInterval(autoI);
-    // },[remainT]);
+    useEffect(()=>{
+        const autoI = setInterval(()=>{
+            setRemainT((remainT)=>remainT - 1);
+        },1000);
+        if(remainT == 0){
+            clearInterval(autoI);
+            timeSts();
+        }
+        return ()=> clearInterval(autoI);
+    },[remainT]);
     return (
         <div className="timer">
             남은시간<em>{remainT}</em>

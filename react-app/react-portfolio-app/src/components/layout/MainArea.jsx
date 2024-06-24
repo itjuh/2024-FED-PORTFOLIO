988991dc50948dbde2bb811da93fab89167ed6b2
itjuh <jh.2144.9679@gmail.com>
2024-06-24 19:35:03 +0900

    pause,stop버튼이동, orderList reset이동
    메뉴수정 따른 리렌더링 영향 제거

--- react-app/react-portfolio-app/src/components/layout/MainArea.jsx
+++ react-app/react-portfolio-app/src/components/layout/MainArea.jsx
@@ -4,38 +4,47 @@ import { useEffect, useLayoutEffect } from "react";
 import { RemainTime } from "../modules/RemainTime";
 import { useNavigate } from "react-router-dom";
 import { menuStore, timeStore } from "../../stores/store";
-import { FaPause, FaPlay } from "react-icons/fa";
 
 export default function MainArea() {
-  console.log("랜더링됨! main");
   // 선택메뉴[tit,link]
-  const { orderList, resetMenu } = menuStore((state) => state);
   // 리랜더링 방지를 위해 필요한 state만 가져옴
+  const { orderList, resetMenu } = menuStore((state) => state);
+  const setOrderList = menuStore((state)=>state.setOrderList);
   const timeEndStatus = timeStore((state) => state.timeEndStatus);
-  const timeStopGo = timeStore((state) => state.timeStopGo);
-  const timePause  = timeStore((state) => state.timePause);
+  console.log("랜더링됨! main",orderList);
+
   // 페이지 이동
   const nav = useNavigate();
+
   useEffect(() => {
-    if (timeEndStatus) {
-      if (orderList) {
-        nav(orderList[1]);
-      } else {
-        nav("profile");
-      }
-    }
-  }, [timeEndStatus,orderList]);
+    // if (timeEndStatus) {
+    //   if (orderList) {
+    //     nav(orderList[1]);
+    //   } else {
+    //     nav("profile");
+    //   }
+    // }
+  }, [timeEndStatus, orderList]);
 
   function goLink() {
     if (orderList) {
       nav(orderList[1]);
     }
   }
+  function delItem(itemName){
+    // 새로운 객체 생성하여 해당 아이템 제외
+    const { [itemName]: deletedItem, ...newOrderList } = orderList;
+    if(Object.keys(newOrderList).length === 0){
+      resetMenu();
+    }else{
+      setOrderList(newOrderList);
+    }
+  }
   return (
     <div className="main">
       <div className="items-area">
         <ul>
-          <Items/>
+          <Items />
         </ul>
       </div>
       <div className="view-area">
@@ -49,26 +58,21 @@ export default function MainArea() {
               자동으로 이동합니다.
             </div>
           )}
-          {orderList !== null && (
+          {orderList !== null &&(
             <>
-              <span className="sel-menu">{orderList[0]}</span>
-              <span className="sel-cnt">수량 1개</span>
-              <div className="alert-box">남은시간 종료 후 선택 상품으로 이동합니다.</div>
+              {Object.keys(orderList).map((itemName, index) => (
+                <div className="sel-group" key={index}>
+                  <span className="sel-menu">{itemName}</span>
+                  <button className="sel-cnt" 
+                    onClick={()=>delItem(itemName)}>삭제</button>
+                </div>
+              ))}
+              <div className="alert-box">시간이 초과되면 삭제됩니다.</div>
             </>
           )}
         </div>
         <div className="info-view">
           <RemainTime />
-          <button className="main-btn pause-btn" onClick={timeStopGo}>
-            {
-                timePause &&
-                <FaPlay />
-            }
-            {
-                !timePause &&
-                <FaPause />
-            }
-          </button>
           <button className="main-btn del-btn over-btn" onClick={resetMenu}>
             삭제하기
           </button>
--- react-app/react-portfolio-app/src/components/modules/RemainTime.jsx
+++ react-app/react-portfolio-app/src/components/modules/RemainTime.jsx
@@ -1,25 +1,38 @@
 import { useEffect } from "react";
-import { timeStore } from "../../stores/store";
+import { menuStore, timeStore } from "../../stores/store";
+import { FaPause, FaPlay } from "react-icons/fa";
 
 export function RemainTime() {
-    // 남은시간
-    const { remainTime , decreaseRemainTime, timeEnd, timePause} = timeStore(state => state);
+  // 남은시간
+  const remainTime = timeStore((state) => state.remainTime);
+  const decreaseRemainTime = timeStore((state) => state.decreaseRemainTime);
+  const timeEnd = timeStore((state) => state.timeEnd);
+  const timeStopGo = timeStore((state) => state.timeStopGo);
+  const timePause = timeStore((state) => state.timePause);
+  const resetMenu = menuStore((state) => state.resetMenu);
 
-    useEffect(()=>{
-        const autoI = setInterval(decreaseRemainTime,1000);
-        if(remainTime == 0){
-            clearInterval(autoI);
-            timeEnd();
-        }
-        if(timePause){
-            clearInterval(autoI);
-        }
-        return ()=> clearInterval(autoI);
-    },[remainTime, timePause]);
-    return (
-        <div className="timer">
-            남은시간<em>{remainTime}</em>
-            <span>초</span>
-        </div>
-    );
+  useEffect(() => {
+    const autoI = setInterval(decreaseRemainTime, 1000);
+    if (remainTime == 0) {
+      clearInterval(autoI);
+      timeEnd();
+      resetMenu();
+    }
+    if (timePause) {
+      clearInterval(autoI);
+    }
+    return () => clearInterval(autoI);
+  }, [remainTime, timePause]);
+  return (
+    <>
+      <div className="timer">
+        남은시간<em>{remainTime}</em>
+        <span>초</span>
+      </div>
+      <button className="main-btn pause-btn" onClick={timeStopGo}>
+        {timePause && <FaPlay />}
+        {!timePause && <FaPause />}
+      </button>
+    </>
+  );
 }
